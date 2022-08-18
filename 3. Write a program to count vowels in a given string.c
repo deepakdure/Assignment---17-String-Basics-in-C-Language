79997dabb8f8af785f6ac709352dd6258ee98574
DEEPAK KUMAR <dksinghdure@outlook.com>
2022-08-18 09:58:11 +0530

    Update 3. Write a program to count vowels in a given string.c

--- 3. Write a program to count vowels in a given string.c
+++ 3. Write a program to count vowels in a given string.c
@@ -8,7 +8,7 @@ int main()
     gets(str);
     for (i = 0; i < str[i]; i++)
     {
-        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
         {
             c++;
         }
